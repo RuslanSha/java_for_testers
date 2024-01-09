a57bdb4692d030a4c8207c19a66648f1c79a87a8
RuslanSha <Sharipov.RF@gazprom-neft.ru>
2024-01-09 12:57:24 +0300

    to #3.1

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,76 +1,16 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import com.titusfortner.logging.SeleniumLogger;
-import java.util.logging.Level;
-
-public class GroupCreationTests {
-    private static WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        SeleniumLogger.enable();
-        SeleniumLogger seleniumLogger = new SeleniumLogger();
-        seleniumLogger.setLevel(Level.WARNING);
-
-        if (driver == null) {
-            driver = new FirefoxDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost:81/addressbook/");
-            driver.manage().window().setSize(new Dimension(1010, 740));
-            driver.findElement(By.name("user")).click();
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).click();
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void canCreateGroup() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("group name");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("group header");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("group footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("groups")).click();
-    }
-
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
+        openGroupsPage();
+        createGroup("group name", "group header", "group footer");
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("groups")).click();
+        openGroupsPage();
+        createGroup("", "", "");
     }
 }
---
+++ addressbook_web_tests/src/test/java/TestBase.java
@@ -0,0 +1,70 @@
+import com.titusfortner.logging.SeleniumLogger;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.logging.Level;
+
+public class TestBase {
+    protected static WebDriver driver;
+
+    protected static void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        SeleniumLogger.enable();
+        SeleniumLogger seleniumLogger = new SeleniumLogger();
+        seleniumLogger.setLevel(Level.WARNING);
+
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost:81/addressbook/");
+            driver.manage().window().setSize(new Dimension(1010, 740));
+            driver.findElement(By.name("user")).click();
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).click();
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
+
+    protected void createGroup(String group_name, String group_header, String group_footer) {
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys(group_name);
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys(group_header);
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(group_footer);
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+    protected void openGroupsPage() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+    }
+
+    protected boolean isGroupPresent() {
+        return isElementPresent(By.name("selected[]"));
+    }
+}
