ab54cf2d79454820168c8385474b9c4758e23d0c
RuslanSha <Sharipov.RF@gazprom-neft.ru>
2024-01-09 17:15:36 +0300

    to #3.5

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,15 +5,22 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ApplicationManager {
     protected static WebDriver driver;
     private LoginHelper session;
-    public GroupHelper groups;
+    private GroupHelper groups;
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new FirefoxDriver();
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost:81/addressbook/");
             driver.manage().window().setSize(new Dimension(1010, 740));
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         if (app == null) {
             app = new ApplicationManager();
         }
-        app.init();
+        app.init(System.getProperty("browser", "firefox"));
     }
 
 }
