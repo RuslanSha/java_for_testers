91b534ed696f0c4ef0092479614f7ab0301e83bf
RuslanSha <Sharipov.RF@gazprom-neft.ru>
2024-01-09 14:00:13 +0300

    to #3.3

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -1,43 +1,28 @@
-import com.titusfortner.logging.SeleniumLogger;
+package manager;
+
 import model.GroupData;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.logging.Level;
-
-public class TestBase {
+public class ApplicationManager {
     protected static WebDriver driver;
 
-    protected static void removeGroup() {
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.name("delete")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    @BeforeEach
-    public void setUp() {
-        SeleniumLogger.enable();
-        SeleniumLogger seleniumLogger = new SeleniumLogger();
-        seleniumLogger.setLevel(Level.WARNING);
-
+    public void init() {
         if (driver == null) {
             driver = new FirefoxDriver();
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost:81/addressbook/");
             driver.manage().window().setSize(new Dimension(1010, 740));
-            driver.findElement(By.name("user")).click();
             driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).click();
             driver.findElement(By.name("pass")).sendKeys("secret");
             driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
         }
     }
 
-    protected boolean isElementPresent(By locator) {
+    public boolean isElementPresent(By locator) {
         try {
             driver.findElement(locator);
             return true;
@@ -46,7 +31,7 @@ public class TestBase {
         }
     }
 
-    protected void createGroup(GroupData group) {
+    public void createGroup(GroupData group) {
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).sendKeys(group.name());
@@ -59,13 +44,19 @@ public class TestBase {
         driver.findElement(By.linkText("group page")).click();
     }
 
-    protected void openGroupsPage() {
+    public void openGroupsPage() {
         if (!isElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
     }
 
-    protected boolean isGroupPresent() {
+    public boolean isGroupPresent() {
         return isElementPresent(By.name("selected[]"));
+    }
+
+    public void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
     }
 }
---
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.titusfortner.logging.SeleniumLogger;
+import manager.ApplicationManager;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.logging.Level;
+
+public class TestBase {
+
+    protected static ApplicationManager app;
+
+    @BeforeEach
+    public void setUp() {
+        SeleniumLogger.enable();
+        SeleniumLogger seleniumLogger = new SeleniumLogger();
+        seleniumLogger.setLevel(Level.WARNING);
+
+        if (app == null) {
+            app = new ApplicationManager();
+        }
+        app.init();
+    }
+
+}
