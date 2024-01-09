96ba713f277262797826a6692c00c56b50d64843
RuslanSha <Sharipov.RF@gazprom-neft.ru>
2024-01-09 13:31:56 +0300

    to #3.2

--- addressbook_web_tests/src/test/java/TestBase.java
+++ addressbook_web_tests/src/test/java/TestBase.java
@@ -1,4 +1,5 @@
 import com.titusfortner.logging.SeleniumLogger;
+import model.GroupData;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -45,17 +46,17 @@ public class TestBase {
         }
     }
 
-    protected void createGroup(String group_name, String group_header, String group_footer) {
+    protected void createGroup(GroupData group) {
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys(group_name);
+        driver.findElement(By.name("group_name")).sendKeys(group.name());
         driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys(group_header);
+        driver.findElement(By.name("group_header")).sendKeys(group.header());
         driver.findElement(By.name("group_footer")).click();
         driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys(group_footer);
+        driver.findElement(By.name("group_footer")).sendKeys(group.footer());
         driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.linkText("group page")).click();
     }
 
     protected void openGroupsPage() {
