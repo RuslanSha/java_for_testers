925fba7618f894b8f5624c4a0e249f58dd8ec1b9
RuslanSha <Sharipov.RF@gazprom-neft.ru>
2024-01-19 17:07:19 +0300

    to#6.3

--- addressbook_web_tests/src/test/java/manager/JdbcHelper.java
+++ addressbook_web_tests/src/test/java/manager/JdbcHelper.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcHelper extends HelperBase {
-
     public JdbcHelper(ApplicationManager manager) {
         super(manager);
     }
